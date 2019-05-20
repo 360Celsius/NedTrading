9e5be9054a4a6643352066ca3118809cc192a0d8
Dennis Shar <dennis.shar@servicenow.com>
2019-05-20 18:12:37 +0300

    new project created

---
+++ Ned/app/src/main/java/com/a360celsius/ned/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.a360celsius.ned;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.a360celsius.ned.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.splash_scree_back_ground));
+
+    }
+}
---
+++ Ned/app/src/main/java/com/a360celsius/ned/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.a360celsius.ned;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import com.a360celsius.ned.databinding.ActivitySplashBinding;
+
+
+public class SplashActivity extends BaseActivity {
+
+    private ActivitySplashBinding activitySplashBinding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+    }
+}
